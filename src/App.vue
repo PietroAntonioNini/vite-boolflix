64d3836e83ab37c9074a4a1f7883c994a613abf3
PietroAntonioNini <151193470+PietroAntonioNini@users.noreply.github.com>
2024-03-06 11:34:26 +0100

    milestone 0

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,28 @@
 <script>
+import axios from 'axios';
+import { store } from './store';
 
+export default {
+    data() {
+        return {
+          store,
+        }
+    },
+
+    created() {
+        axios.get('https://api.themoviedb.org/3/search/movie?api_key=9b7fdd843817417b0e4e84b2c0542c07&query=mercenari').then(res => {
+          console.log(res.data.results);
+        });
+    },
+
+    components: {
+
+    },
+
+    methods: {
+
+    },
+}
 </script>
 
 <template>
